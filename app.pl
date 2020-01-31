95804306ba35a033706b9a73915b39264ab84142
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2020-01-31 03:45:18 -0300

    Switch auth type

--- app.pl
+++ app.pl
@@ -3,20 +3,20 @@ use strict;
 use warnings;
 use Mojolicious::Lite;
 use Mojo::JSON qw( decode_json );
+use Mojo::Util qw( secure_compare );
 
 get '/' => sub {
   my $c        = shift;
   my $json     = $c->req->json;
-  my $email    = $json->{email};
-  my $password = $json->{password};
-  my $cmd      = `mongo --quiet --eval 'JSON.stringify(db.serverStatus())' -u clustermonitor -p "$ENV{'SKY_PA'}"`;
 
-  unless($email eq $ENV{'SKY_EM'} && $password eq $ENV{'SKY_PA'}) {
+  unless(secure_compare $c->req->url->to_abs->userinfo, "$ENV{'SKY_EM'}:$ENV{'SKY_PA'}") {
     return $c->render(
-      json   => {error => 'invalid_username_or_password'},
-      status => 400
+      json   => {error => 'unauthorized'},
+      status => 401
     );
-  }
+  }    
+
+  my $cmd = `mongo --quiet --eval 'JSON.stringify(db.serverStatus())' -u clustermonitor -p "$ENV{'SKY_PA'}"`;
 
   return $c->render(
     json   => decode_json $cmd,
